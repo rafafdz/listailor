73af8f0b7c7f53eb43fe43d48def80ade0117ee7
Rafael Fernández <rafafdz01@gmail.com>
2024-08-30 01:03:51 -0400

    remove base posthog

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -5,7 +5,6 @@ import { Inter as FontSans } from 'next/font/google';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ListsProvider } from '@/contexts/ListsContext';  // Import the provider
 import { Analytics } from "@vercel/analytics/react";
-import { PostHogProvider } from '@/components/PostHogProvider';  // Import the new component
 
 const fontSans = FontSans({
   subsets: ['latin'],
@@ -18,23 +17,21 @@ function App({ Component, pageProps }) {
   return (
     <QueryClientProvider client={queryClient}>
       <ListsProvider>
-        <PostHogProvider>
-          <Head>
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-            />
-          </Head>
-          <Analytics />
-          <div
-            className={`
-              min-h-screen flex flex-col font-sans antialiased
-              ${fontSans.variable}
-            `}
-          >
-            <Component {...pageProps} />
-          </div>
-        </PostHogProvider>
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          />
+        </Head>
+        <Analytics />
+        <div
+          className={`
+            min-h-screen flex flex-col font-sans antialiased
+            ${fontSans.variable}
+          `}
+        >
+          <Component {...pageProps} />
+        </div>
       </ListsProvider>
     </QueryClientProvider>
   );
